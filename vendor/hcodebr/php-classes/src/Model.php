1786c697139884c2049d4d493741ca7546b19ef9
marcia <marcia.dasilva1410@gmail.com>
2017-09-12 10:24:54 -0300

    finalmente funcionando crud admin

--- vendor/hcodebr/php-classes/src/Model.php
+++ vendor/hcodebr/php-classes/src/Model.php
@@ -2,35 +2,48 @@
 
 namespace Hcode;
 
-class Model{
+class Model {
 
     private $values = [];
 
-    public function __call($name, $args){
+    public function __call($name, $args)
+    {
+
         $method = substr($name, 0, 3);
         $fieldName = substr($name, 3, strlen($name));
 
-        switch ($method){
+        switch ($method)
+        {
+
             case "get":
-                return $this->values[$fieldName];
+                return (isset($this->values[$fieldName])) ? $this->values[$fieldName] : NULL;
                 break;
 
             case "set":
                 $this->values[$fieldName] = $args[0];
                 break;
+
         }
+
     }
 
-    public function setData($data = array()){
-        foreach ($data as $key => $value){
+    public function setData($data = array())
+    {
+
+        foreach ($data as $key => $value) {
+
             $this->{"set".$key}($value);
+
         }
+
     }
 
-    public function getValues(){
+    public function getValues()
+    {
+
         return $this->values;
-    }
 
+    }
 
 }
 
