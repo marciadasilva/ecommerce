e81a744942091cc1809fc9f45448c4ef0ff055da
Marcia <Marcia>
2017-09-12 21:41:51 -0300

    esqueci minha senha - não funciona parte de enviar email

--- index.php
+++ index.php
@@ -128,7 +128,7 @@ $app->post("/admin/users/create", function() {
 
     $_POST["inadmin"] = (isset($_POST["inadmin"]))?1:0;
 
-    /*$_POST['despassword'] = User::getPassswordHash($_POST['despassword']);*/
+    $_POST['despassword'] = User::getPassswordHash($_POST['despassword']);
 
     $user->setData($_POST);
 
@@ -155,6 +155,76 @@ $app->post("/admin/users/:iduser", function($iduser) {
 
     header("Location: /admin/users");
     exit;
+
+});
+
+$app->get("/admin/forgot", function() {
+
+    $page = new PageAdmin([
+        "header"=>false,
+        "footer"=>false
+    ]);
+
+    $page->setTpl("forgot");
+
+});
+
+$app->post("/admin/forgot", function(){
+
+    $user = User::getForgot($_POST["email"]);
+
+    header("Location: /admin/forgot/sent");
+    exit;
+
+});
+
+$app->get("/admin/forgot/sent", function(){
+
+    $page = new PageAdmin([
+        "header"=>false,
+        "footer"=>false
+    ]);
+
+    $page->setTpl("forgot-sent");
+
+});
+
+$app->get("/admin/forgot/reset", function(){
+
+    $user = User::validForgotDecrypt($_GET["code"]);
+
+    $page = new PageAdmin([
+        "header"=>false,
+        "footer"=>false
+    ]);
+
+    $page->setTpl("forgot-reset", array(
+        "name"=>$user["desperson"],
+        "code"=>$_GET["code"]
+    ));
+
+});
+
+$app->post("/admin/forgot/reset", function(){
+
+    $forgot = User::validForgotDecrypt($_POST["code"]);
+
+    User::setFogotUsed($forgot["idrecovery"]);
+
+    $user = new User();
+
+    $user->get((int)$forgot["iduser"]);
+
+    $password = User::getPasswordHash($_POST["password"]);
+
+    $user->setPassword($password);
+
+    $page = new PageAdmin([
+        "header"=>false,
+        "footer"=>false
+    ]);
+
+    $page->setTpl("forgot-reset-success");
 
 });
 
